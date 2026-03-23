29a0db557134f9857496d660ac5747e1deac72ed
sandesh <sandesh.durgekar@egovernments.org>
2026-03-23 15:19:09 +0530

    Revert "added label text end of lat long"
    This reverts commit 5b499cd1fcb5a1bfb7b1060478fd64abfec3a28a.

--- apps/health_campaign_field_worker_app/lib/sampleJsonConfigs/registration_flows.dart
+++ apps/health_campaign_field_worker_app/lib/sampleJsonConfigs/registration_flows.dart
@@ -873,7 +873,7 @@ final dynamic sampleFlows = {
                   },
                   {
                     "type": "template",
-                    "label": "{{fn:getInEligibleStatus(item.task)}}",
+                    "label": "NOT_ELIGIBLE",
                     "format": "tag",
                     "visible":
                         "{{fn:checkEligibilityForAgeAndSideEffect(item.individual.0.dateOfBirth, item.task, contextData.0.currentRunningCycle)}}==false",
@@ -1350,33 +1350,8 @@ final dynamic sampleFlows = {
             },
             {
               "type": "string",
-              "label": "APPONE_UNABLETODELIVER_LATLNG_LABEL",
-              "order": 2,
-              "value": "",
-              "format": "latLng",
-              "hidden": false,
-              "isMdms": false,
-              "tooltip": "",
-              "helpText": "",
-              "infoText": "",
-              "readOnly": false,
-              "fieldName": "latLng",
-              "mandatory": false,
-              "showLabel": false,
-              "deleteFlag": false,
-              "innerLabel": "",
-              "schemaCode": null,
-              "systemDate": false,
-              "validations": [],
-              "errorMessage": "",
-              "includeInForm": true,
-              "isMultiSelect": false,
-              "includeInSummary": true,
-            },
-            {
-              "type": "string",
               "label": "UNABLETODELIVERY_FLOW_comment_LABEL",
-              "order": 3,
+              "order": 2,
               "value": "",
               "format": "textArea",
               "hidden": false,
@@ -2564,7 +2539,7 @@ final dynamic sampleFlows = {
             {
               "type": "string",
               "label": "APPONE_REGISTRATION_DELIVERYDETAILS_label_scanner",
-              "order": 5,
+              "order": 4,
               "value": "",
               "format": "scanner",
               "hidden": false,
@@ -2585,31 +2560,6 @@ final dynamic sampleFlows = {
               "includeInForm": true,
               "isMultiSelect": false,
               "includeInSummary": true
-            },
-            {
-              "type": "string",
-              "label": "APPONE_DELIVERYDETAILS_LATLNG_LABEL",
-              "order": 4,
-              "value": "",
-              "format": "latLng",
-              "hidden": false,
-              "isMdms": false,
-              "tooltip": "",
-              "helpText": "",
-              "infoText": "",
-              "readOnly": false,
-              "fieldName": "latLng",
-              "mandatory": false,
-              "showLabel": false,
-              "deleteFlag": false,
-              "innerLabel": "",
-              "schemaCode": null,
-              "systemDate": false,
-              "validations": [],
-              "errorMessage": "",
-              "includeInForm": true,
-              "isMultiSelect": false,
-              "includeInSummary": true,
             }
           ],
           "actionLabel":
@@ -3359,8 +3309,7 @@ final dynamic sampleFlows = {
                     "key": "ProjectBeneficiaryClientReferenceId",
                     "value":
                         "{{navigation.ProjectBeneficiaryClientReferenceId}}"
-                  },
-                  {"key": "status", "value": "INELIGIBLE"}
+                  }
                 ],
                 "onError": [
                   {
@@ -3722,6 +3671,7 @@ final dynamic sampleFlows = {
               "mandatory": true,
               "deleteFlag": false,
               "innerLabel": "",
+              "includeInForm": true,
               "schemaCode": "HCM.ID_TYPE_OPTIONS_POPULATOR",
               "systemDate": false,
               "validations": [
@@ -4982,6 +4932,10 @@ final dynamic sampleFlows = {
               "readOnly": false,
               "required": true,
               "fieldName": "gender",
+              "enums": [
+                {"code": "MALE", "name": "MALE"},
+                {"code": "FEMALE", "name": "Female"}
+              ],
               "mandatory": true,
               "deleteFlag": false,
               "innerLabel": "",
