56dab2caeca9776335304d3e895451b1e9edf60d
aviral-beehyv <aviral.gupta@beehyv.com>
2026-05-12 15:15:14 +0530

    remove visibility condition for stock loss report and update version to 2.2.30

--- apps/health_campaign_field_worker_app/lib/sampleJsonConfigs/inventory_reports.dart
+++ apps/health_campaign_field_worker_app/lib/sampleJsonConfigs/inventory_reports.dart
@@ -183,7 +183,6 @@ final dynamic inventoryReportFlows = {
           "type": "template",
           "format": "menu_card",
           "heading": "STOCKREPORTS_VIEW_REPORTS_STOCK_LOSS_HEADING",
-          "visible": "{{fn:hasRole('WAREHOUSE_MANAGER')}} == false",
           "description": "STOCKREPORTS_VIEW_REPORTS_STOCK_LOSS_DESCRIPTION",
           "icon": "Assessment",
           "onAction": [
