d1cffa5763c6b91f552944404250b2485951c219
egov-skb <susanta.behera@egov.global>
2026-05-21 16:10:36 +0530

    Added new method to for beneficiary downsync

--- apps/health_campaign_field_worker_app/lib/data/repositories/remote/downsync.dart
+++ apps/health_campaign_field_worker_app/lib/data/repositories/remote/downsync.dart
@@ -1,6 +1,7 @@
 // Generated using mason. Do not modify by hand
 
 import 'package:digit_data_model/data_model.dart';
+import 'package:dio/dio.dart';
 import '../../../models/downsync/downsync.dart';
 
 class DownsyncRemoteRepository
@@ -13,4 +14,54 @@ class DownsyncRemoteRepository
 
   @override
   DataModelType get type => DataModelType.downsync;
+
+  @override
+  Future<Map<String, dynamic>> downSync(
+    DownsyncSearchModel query, {
+    int? offSet,
+    int? limit,
+  }) async {
+    final response = await executeFuture<Response>(
+      future: () async {
+        return dio.post(
+          searchPath,
+          queryParameters: {
+            'offset': offSet ?? 0,
+            'limit': limit ?? 100,
+            'tenantId': DigitDataModelSingleton().tenantId,
+            if (query.isDeleted ?? false) 'includeDeleted': query.isDeleted,
+          },
+          data: {
+            'DownsyncCriteria': query.toMap(),
+          },
+        );
+      },
+    );
+
+    final responseMap = response.data;
+    if (responseMap is! Map<String, dynamic>) {
+      throw InvalidApiResponseException(
+        data: query.toMap(),
+        path: searchPath,
+        response: responseMap,
+      );
+    }
+
+    if (responseMap.containsKey('DownloadLinks')) {
+      return responseMap;
+    }
+
+    if (responseMap.containsKey(entityName)) {
+      final entityResponse = responseMap[entityName];
+      if (entityResponse is Map<String, dynamic>) {
+        return entityResponse;
+      }
+    }
+
+    throw InvalidApiResponseException(
+      data: query.toMap(),
+      path: searchPath,
+      response: responseMap,
+    );
+  }
 }
