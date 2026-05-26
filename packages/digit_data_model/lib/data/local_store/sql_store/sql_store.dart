c0350f562447639cb4564e8130a0ba3e6ac7941f
psbskb <susanta.behera@equidhi.org>
2026-05-26 16:35:04 +0530

    keep the schemaVersion as 9

--- packages/digit_data_model/lib/data/local_store/sql_store/sql_store.dart
+++ packages/digit_data_model/lib/data/local_store/sql_store/sql_store.dart
@@ -144,7 +144,7 @@ class LocalSqlDataStore extends _$LocalSqlDataStore {
 
   /// The `schemaVersion` getter returns the schema version of the database.
   @override
-  int get schemaVersion => 10; // Increment schema version
+  int get schemaVersion => 9; // Increment schema version
 
   Future<void> _createTaskSearchIndexes() async {
     await customStatement('''
