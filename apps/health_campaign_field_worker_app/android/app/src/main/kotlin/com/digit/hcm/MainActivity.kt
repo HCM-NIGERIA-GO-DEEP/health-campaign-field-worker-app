3ea1735e38de3716db010c54df5e9a1a19dbc9f3
amitverma-egov <amit.verma@egovernments.org>
2026-06-02 14:51:43 +0530

    chore:update package name from hcm to kg - for kogi

--- apps/health_campaign_field_worker_app/android/app/src/main/kotlin/com/digit/hcm/MainActivity.kt
+++ apps/health_campaign_field_worker_app/android/app/src/main/kotlin/com/digit/hcm/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.digit.hcm
+package org.egov.kg
 
 import android.content.BroadcastReceiver
 import android.content.Context
--- apps/health_campaign_field_worker_app/lib/firebase_options.dart
+++ apps/health_campaign_field_worker_app/lib/firebase_options.dart
@@ -51,7 +51,7 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyBzE5f156_ia7HumLb1fDg8A-88uhmQ68Y',
-    appId: '1:397617237644:android:ef1103344485f51af6b5a9',  // com.digit.hcm
+    appId: '1:397617237644:android:ef1103344485f51af6b5a9', // org.egov.kg
     messagingSenderId: '397617237644',
     projectId: 'digit-health-75ed0',
     storageBucket: 'digit-health-75ed0.appspot.com',
@@ -59,11 +59,12 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyAcvb5-mL5VJA5hbm2Detexa01ZDt48z-U',
-    appId: '1:397617237644:ios:6711bca5cf278891f6b5a9',
+    appId: '1:397617237644:android:f88dc37ea312cf5ff6b5a9',
     messagingSenderId: '397617237644',
     projectId: 'digit-health-75ed0',
     storageBucket: 'digit-health-75ed0.appspot.com',
-    iosClientId: '397617237644-ddc7e9g08v752477td0vpl72it6sfpam.apps.googleusercontent.com',
+    iosClientId:
+        '397617237644-ddc7e9g08v752477td0vpl72it6sfpam.apps.googleusercontent.com',
     iosBundleId: 'org.egov.salama',
   );
 }
