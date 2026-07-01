0c97151c879cfff6d1501ce16b76bf173c0cf2d6
Harsh Khatri <harsh.khatri@beehyv.com>
2026-07-01 13:18:11 +0530

    SIlent API call the fetch the id from the downsync id api call.

--- apps/health_campaign_field_worker_app/lib/executors/load_unique_id_pool_executor.dart
+++ apps/health_campaign_field_worker_app/lib/executors/load_unique_id_pool_executor.dart
@@ -1,3 +1,6 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:digit_data_model/data/repositories/package_repository/remote/unique_id_pool.dart';
 import 'package:digit_data_model/data_model.dart';
 import 'package:digit_data_model/models/entities/id_status.dart';
 import 'package:digit_flow_builder/action_handler/action_config.dart';
@@ -7,6 +10,10 @@ import 'package:digit_flow_builder/utils/interpolation.dart';
 import 'package:digit_flow_builder/widget_registry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../blocs/app_initialization/app_initialization.dart';
+import '../utils/environment_config.dart';
+import '../utils/extensions/extensions.dart';
 
 class LoadUniqueIdPoolExecutor extends ActionExecutor {
   @override
@@ -27,19 +34,72 @@ class LoadUniqueIdPoolExecutor extends ActionExecutor {
     try {
       final repository = context
           .read<LocalRepository<UniqueIdPoolModel, UniqueIdPoolSearchModel>>();
+      final remoteRepository = context.read<UniqueIdPoolRemoteRepository>();
+      final userUuid = context.loggedInUserUuid;
 
-      final searchResult = repository.search(UniqueIdPoolSearchModel(
-        status: IdStatus.unAssigned.toValue(),
-      ));
+      Future<List<UniqueIdPoolModel>> getAvailableIds() async {
+        final searchResult = repository.search(UniqueIdPoolSearchModel(
+          status: IdStatus.unAssigned.toValue(),
+          userUuid: userUuid,
+        ));
 
-      List<UniqueIdPoolModel> availableIds;
-      if (searchResult is Future) {
-        availableIds = await (searchResult as Future<List<UniqueIdPoolModel>>);
-      } else {
-        availableIds = List<UniqueIdPoolModel>.from(searchResult as List);
+        if (searchResult is Future) {
+          return await (searchResult as Future<List<UniqueIdPoolModel>>);
+        } else {
+          return List<UniqueIdPoolModel>.from(searchResult as List);
+        }
       }
 
-      final count = availableIds.length;
+      List<UniqueIdPoolModel> availableIds = await getAvailableIds();
+      var count = availableIds.length;
+
+      if (count == 0) {
+        try {
+          final connectivityResult = await Connectivity().checkConnectivity();
+          if (!context.mounted) return contextData;
+          if (!connectivityResult.contains(ConnectivityResult.none)) {
+            final appInitializationState =
+                context.read<AppInitializationBloc>().state;
+            int batchSize = 10;
+            if (appInitializationState is AppInitialized) {
+              batchSize = appInitializationState
+                      .appConfiguration.beneficiaryIdConfig?.first.batchSize
+                      .toInt() ??
+                  10;
+            }
+
+            final tenantId = envConfig.variables.tenantId;
+            final deviceInfo = DeviceInfoPlugin();
+            final androidInfo = await deviceInfo.androidInfo;
+            final deviceUuid = androidInfo.id;
+
+            final searchModel = UniqueIdPoolSearchModel(
+              deviceInfo: androidInfo.toString(),
+              userUuid: userUuid,
+              deviceUuid: deviceUuid,
+              tenantId: tenantId,
+              count: batchSize,
+              fetchAllocatedIds: false,
+            );
+
+            final response = await remoteRepository.searchWithMetadata(
+              searchModel,
+              limit: batchSize,
+              offSet: 0,
+            );
+
+            final List<UniqueIdPoolModel> batch = response.models;
+            if (batch.isNotEmpty) {
+              await repository.bulkCreate(batch);
+              availableIds = await getAvailableIds();
+              count = availableIds.length;
+            }
+          }
+        } catch (e) {
+          debugPrint('Silent beneficiary ID downsync failed: $e');
+        }
+      }
+
       UniqueIdPoolModel? latestIdModel;
       if (availableIds.isNotEmpty) {
         availableIds.sort((a, b) {
