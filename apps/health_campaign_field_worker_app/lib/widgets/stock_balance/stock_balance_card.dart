63cb1de2aa521a03c7800f2e3732a567a20ed2c1
egov-skb <susanta.behera@egov.global>
2026-07-20 01:30:08 +0530

    feat: integrate server summary report service for stock and household data filtering

--- apps/health_campaign_field_worker_app/lib/widgets/stock_balance/stock_balance_card.dart
+++ apps/health_campaign_field_worker_app/lib/widgets/stock_balance/stock_balance_card.dart
@@ -258,7 +258,6 @@ class _StockBalanceCardState extends LocalizedState<StockBalanceCard> {
     final selectedCycle = context.selectedCycle;
 
     final summaryReportService = context.read<ServerSummaryReportService>();
-
     int? serverReportTimestamp = await summaryReportService.timestamp();
     final serverReportStockConsumedMap =
         await summaryReportService.stockConsumedMap();
