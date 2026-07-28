cc4c71cbafc0dc41ca278e659e3042444fc7462a
egov-skb <susanta.behera@egov.global>
2026-07-28 12:23:52 +0530

    fix: set campaign ID for SMC-RI project type in ProjectBloc

--- apps/health_campaign_field_worker_app/lib/blocs/project/project.dart
+++ apps/health_campaign_field_worker_app/lib/blocs/project/project.dart
@@ -812,6 +812,9 @@ class ProjectBloc extends Bloc<ProjectEvent, ProjectState> {
       String? campaignID = event.model.referenceID;
       var projectTypeString = event.model.projectType;
 
+      if (projectTypeString == "SMC-RI") {
+        campaignID = "CMP-2026-06-08-000333";
+      }
       if (projectTypeString == "ORS-Zinc") {
         campaignID = "CMP-2026-07-03-000424";
       }
